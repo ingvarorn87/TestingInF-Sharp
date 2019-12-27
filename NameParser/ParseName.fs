c7da5093f3d08080100f50b55f4bf73bf45f0614
ingvarorn87 <ingvarorn87@gmail.com>
2019-12-27 17:35:11 +0100

    Initial tests added plus xunit for unit testing

---
+++ NameParser/ParseName.fs
@@ -0,0 +1,13 @@
+﻿namespace NameParser
+
+open System
+
+module Parser =
+    
+    let parseName rawName = 
+        
+        if rawName = null then
+            raise <| ArgumentNullException()
+        else
+             raise <| ArgumentException()
+
---
+++ NameParserTest/ParseName.Tests.fs
@@ -0,0 +1,26 @@
+﻿namespace NameParser
+
+open System
+open Xunit
+open NameParser.Parser
+
+module Tests = 
+
+    [<Fact>]
+    let ``I can write a test``() =
+        let expected = true
+        let actual = true
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``A null string produces an argument null exeption``() =
+        let rawName : string = null
+        Assert.Throws(typeof<System.ArgumentNullException>,
+        (fun () -> parseName rawName |> ignore))
+
+    [<Fact>]
+       let ``An empty string produces an argument exeption``() =
+           let rawName : string = ""
+           Assert.Throws(typeof<System.ArgumentException>,
+           (fun () -> parseName rawName |> ignore))
+            
