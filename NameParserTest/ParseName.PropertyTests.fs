d15420e3800d6b42a899df70aca5dc4482cbe99e
ingvarorn87 <ingvarorn87@gmail.com>
2019-12-31 12:33:01 +0100

    Added property test using FsCheck

---
+++ NameParserTest/ParseName.PropertyTests.fs
@@ -0,0 +1,12 @@
+﻿module ParseName
+
+open System
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+
+module PropertyTests =
+
+    [<Property>]
+    let ``A string doesn't contain a letter Z`` (s: string) =
+        s.Contains"Z"|> not
