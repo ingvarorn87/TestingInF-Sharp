7e5a2209dbac38e8b3575fbea148b104e221bf79
ingvarorn87 <ingvarorn87@gmail.com>
2020-01-01 23:02:23 +0100

    New Canopy project added

--- CanopyExample2/Tests.fs
+++ CanopyExample2/Tests.fs
@@ -1,2 +1,12 @@
 ﻿module Tests
 
+open Xunit
+open canopy.parallell
+
+[<Fact>]
+let ``Hightlight Olivercoding Image Test`` () =
+    use browser = functions.start canopy.types.Chrome
+    functions.url "https://www.olivercoding.com/" browser
+    functions.highlight "#mainLogoDiv" browser
+    functions.sleep 5
+    Assert.True(true)
