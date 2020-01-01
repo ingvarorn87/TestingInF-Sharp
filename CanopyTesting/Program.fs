ec9529b680fec045db8cbc982a5f7b1081a51449
ingvarorn87 <ingvarorn87@gmail.com>
2020-01-01 21:33:18 +0100

    First Canopy test ready

--- CanopyTesting/Program.fs
+++ CanopyTesting/Program.fs
@@ -3,14 +3,56 @@
 module Main =
 
     open System
-    open canopy
     open canopy.runner.classic
     open canopy.configuration
     open canopy.classic
     open OpenQA.Selenium
 
-
+    module Initialize =
+           chromeDir <- @"C:/"
+           start chrome
 
     [<AutoOpen>]
-    module Selector
+  
+     module Selectors = 
+          let ``build a bouquet`` = "#nav_main > ul > li:nth-child(3) > a > span"
+          let ``calla lillies`` = "#bab_grid > li:nth-child(1) > a > span > img" 
+          let ``color`` = "#colorCL"
+          let ``qty`` = "#qtyCL"
+          let ``add to basket`` = "#bab_form > input.btn.checkout"
+          let ``basket total`` = "#drawer_grand_total"
+
+   
+
+    "I can order a bouquet" &&& fun _ -> 
+
+        url @"http://585248.youcanlearnit.net/improved"
+
+        click ``build a bouquet``
+        click ``calla lillies``
+        click ``color``
+
+        press Keys.ArrowDown
+
+        click ``qty``
+
+        press Keys.Backspace
+        press Keys.NumberPad2
+        press Keys.Enter
+
+        click ``add to basket``
+        
+        ``basket total`` == "$60.00"
+
+        run()
+
+        printfn "Press [Enter] to exit"
+        Console.ReadKey() |> ignore
+
+        quit()
+
+
+
+
+
 
