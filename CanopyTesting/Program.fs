cc5a550ef033a88d88619964accf8162ced725ef
ingvarorn87 <ingvarorn87@gmail.com>
2020-01-01 20:38:14 +0100

    Canopy testing framework added + selenium libraries

---
+++ CanopyTesting/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module Main =
+
+    open System
+    open canopy
+    open canopy.runner.classic
+    open canopy.configuration
+    open canopy.classic
+    open OpenQA.Selenium
+
+
+
+    [<AutoOpen>]
+    module Selector
+
